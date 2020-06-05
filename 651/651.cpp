45587a58aa788b45261a2942030dc6cf0d70982b
1356602646 <1356602646@qq.com>
2020-06-05 14:58:16 +0800

    651.cpp

--- 651/651.cpp
+++ 651/651.cpp
@@ -1,20 +1,57 @@
-﻿// 651.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
 
-#include <iostream>
+int createMaskByKmeans(cv::Mat src, cv::Mat & mask)
+{
+	if ((mask.type() != CV_8UC1)
+		|| (src.size() != mask.size())
+		) {
+		return 0;
+	}
 
+	int width = src.cols;
+	int height = src.rows;
+
+	int pixNum = width * height;
+	int clusterCount = 2;
+	Mat labels;
+	Mat centers;
+	//数据
+	Mat sampleData = src.reshape(3, pixNum);
+	Mat km_data;
+
+	sampleData.convertTo(km_data, CV_32F);
+	//kmeans
+	TermCriteria criteria = TermCriteria(TermCriteria::EPS + TermCriteria::COUNT, 10, 0.1);
+
+	kmeans(km_data, clusterCount, labels, criteria, clusterCount, KMEANS_PP_CENTERS, centers);
+	//mask
+	uchar fg[2] = { 0,255 };
+	for (int row = 0; row < height; row++) {
+
+		for (int col = 0; col < width; col++) {
+
+			mask.at<uchar>(row, col) = fg[labels.at<int>(row*width + col)];
+
+		}
+
+	}
+
+	return 0;
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat ram = imread("D:\\360downloads\\33.png");
+	Mat chang = Mat::zeros(ram.size(), CV_8UC1);
+
+	createMaskByKmeans(ram, chang);
+
+	imshow("ram", ram);
+
+	imshow("chang", chang);
+
+	waitKey(0);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
